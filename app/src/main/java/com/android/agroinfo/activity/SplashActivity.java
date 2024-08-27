3f00c257e86ceb8547c798dd4bc3fedaa8016b0e
amo95 <jameaamo@gmail.com>
2024-08-27 20:44:42 +0000

    added splash screen

---
+++ app/src/main/java/com/android/agroinfo/activity/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.android.agroinfo.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.agroinfo.R;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 3000; // Duration for the splash screen (in milliseconds)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, SPLASH_DURATION);
+    }
+}
